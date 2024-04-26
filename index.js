94f893be0e1c6fea09432092c7c0fc5557b48f16
Afroza5848 <suriyaafroj5848@gmail.com>
2024-04-26 09:49:20 +0600

    connect mongoDB database

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+// timberZone
+// 9o4tFpMeAo3L9xzI
 
 
-app.get('/', (req,res) => {
+
+//const uri = "mongodb+srv://<username>:<password>@cluster0.uc5r0l2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://timberZone:9o4tFpMeAo3L9xzI@cluster0.uc5r0l2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+       // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('jute and timberZone server is running')
 })
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`timberZone server create on port:${port}`);
 })
