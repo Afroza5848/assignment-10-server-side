65e58bd36bb15c0fdf2f1c5e847f584e56b1846f
Afroza5848 <suriyaafroj5848@gmail.com>
2024-04-28 14:25:11 +0600

    by user email to get the data from the database

--- index.js
+++ index.js
@@ -40,9 +40,16 @@ async function run() {
 
         app.get('/items/:id', async(req,res) => {
             const id = req.params.id;
-            const query = new ObjectId(id);
+            const query = {_id: new ObjectId(id)};
             const result = await itemsCollection.findOne(query);
             res.send(result);
+        })
+        app.get('/myCraftList/:email', async(req,res) => {
+            const email = req.params.email;
+            const query = {user_email: email};
+            const result = await itemsCollection.find(query).toArray();
+            res.send(result)
+            
         })
 
         app.post("/items" , async(req,res) => {
